d5a399be366de64d7dacc1faa8cfbfe3a4f751f3
venuscupid <venuscupid@gmail.com>
2013-09-18 00:31:43 -0500

    first commit from eclipse for the new project

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world! try create a project in eclipse and upload to Github.");
+	}
+
+}
